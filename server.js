d8db1343cfe94c9a8e183d21602bab2ebeefcb4b
victorcas30 <vcastillo.mancia@gmail.com>
2023-08-16 22:05:44 -0600

    config BD

--- server.js
+++ server.js
@@ -4,21 +4,20 @@ import { alumnosRouter } from './api/alumnos/alumnosroutes.js';
 import { empleadosRouter } from './api/empleados/empleadosroutes.js';
 import { errorHandler } from './config/errorHandler.js';
 import cors from 'cors';
+
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-const port = process.env.SERVER_PORT;
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api',alumnosRouter);
+// Utiliza el puerto 3000 si no se ha configurado en las variables de entorno
+const port = process.env.SERVER_PORT || 3000;
+
+app.use('/api', alumnosRouter);
 app.use('/api', empleadosRouter);
 
-//app.use(errorHandler);
-
-app.listen(port,()=>{
-    console.log('server running on port '+port);
+// Escucha en todas las interfaces de red (0.0.0.0) en lugar de localhost (127.0.0.1)
+app.listen(port, '0.0.0.0', () => {
+    console.log('Server running on port ' + port);
 });
-
-
-
